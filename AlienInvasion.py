4e1f2b43e2437855bd40c40275e7efbf60f739b0
Lyndsey <lyndsey_davis2@baylor.edu>
2022-02-28 21:00:01 -0600

    create fleet of aliens

--- AlienInvasion.py
+++ AlienInvasion.py
@@ -46,21 +46,30 @@ class AlienInvasion:
     def _create_fleet(self):
         # make an alien
         alien = Alien(self)
-        # calculate number of aliens and spacing between each
-        alien_width = alien.rect.width
+        # calculate number of aliens and spacing between each per row
+        alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = available_space_x // (2 * alien_width)
 
-        # create first row of aliens
-        for alien_number in range(number_aliens_x):
-            self._create_alien(alien_number)
+        # calculate number of rows
+        ship_height = self.ship.rect.height
+        available_space_y = (
+            self.settings.screen_height - (3 * alien_height) - ship_height
+        )
+        number_rows = available_space_y // (2 * alien_height)
 
-    def _create_alien(self, alien_number):
+        # create full fleet of aliens
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
         # create alien and place in row
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
     # respond to keypresses and mouse events
